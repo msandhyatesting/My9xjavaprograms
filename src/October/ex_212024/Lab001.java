5f59151b83b0a71ef314b8acb90ad3d83f782133
msandhyatesting <mudigonda.sandhya@gmail.com>
2024-10-21 14:54:43 -0700

    my first java program

---
+++ src/October/ex_212024/Lab001.java
@@ -0,0 +1,7 @@
+package October.ex_212024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/October/ex_212024/lab002.java
@@ -0,0 +1,12 @@
+package October.ex_212024;
+
+public class lab002 {
+    public static void main(String[] args) {
+        //this is my program about comments
+        /*these are multi line comments
+*add code explanation here
+         */
+        System.out.println("this program is  about comments");
+    }
+}
+
