10eac4331ac5ef66512186b2c32411005e16e589
msandhyatesting <mudigonda.sandhya@gmail.com>
2024-10-23 17:51:28 -0700

    console arguments

---
+++ src/October/ex_232024/lab023.java
@@ -0,0 +1,10 @@
+package October.ex_232024;
+
+public class lab023 {
+    //console arguments
+    public static void main(String[] args) {
+        int age=args[0];
+        int age=integer.parseInt(args[0]);
+
+    }
+}
